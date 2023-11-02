ad8eb08ea9414845d5d71d5d1d71189665b3e1d7
Daria2906 <daria.chebotnyagina@testpro.io>
2023-11-02 14:27:44 -0500

    c

--- src/test/java/LoginTests.java
+++ src/test/java/LoginTests.java
@@ -1,25 +1,91 @@
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.chrome.ChromeDriver;
-import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebElement;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
-import java.time.Duration;
-
 public class LoginTests extends BaseTest {
+
     @Test
     public void loginEmptyEmailPassword() {
 
 //      Added ChromeOptions argument below to fix websocket error
-        ChromeOptions options = new ChromeOptions();
-        options.addArguments("--remote-allow-origins=*");
+navigateToLoginPage();
 
-        WebDriver driver = new ChromeDriver(options);
-        driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
-
-        String url = "https://qa.koel.app/";
-        driver.get(url);
         Assert.assertEquals(driver.getCurrentUrl(), url);
         driver.quit();
     }
+
+    @Test
+    public void loginValidEmailPassword (){
+        //Pre-condition: Chrome browser should be opened
+
+        // Step 1: Open Koel login page
+    navigateToLoginPage();
+
+        // Step 2: Enter existing username (email)
+    provideEmail("daria.chebotnyagina@testpro.io");
+
+        // Step 3: Enter correct password
+    providePassword("Asdfasdf1");
+
+        // Step 4: Click login button
+    clickSubmit();
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+
+        // Expected result
+        Assert.assertTrue(avatar.isDisplayed());
+        //driver.quit();
+    }
+
+    @Test
+    //Negative test
+    public void loginInvalidEmailValidPassword (){
+        //Pre-condition: Chrome browser should be opened
+
+        // Step 1: Open Koel login page
+    navigateToLoginPage();
+
+        // Step 2: Enter existing username (email)
+    provideEmail("invalidemail@testpro.io");
+
+        // Step 3: Enter correct password
+    providePassword("Asdfasdf1");
+
+        // Step 4: Click login button
+    clickSubmit();
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+
+        // Expected result
+        Assert.assertTrue(avatar.isDisplayed());
+        //driver.quit();
+    }
+
+    @Test
+    //Negative test
+    public void loginValidEmailNoPassword (){
+        //Pre-condition: Chrome browser should be opened
+
+        // Step 1: Open Koel login page
+    navigateToLoginPage();
+
+        // Step 2: Enter existing username (email)
+    provideEmail("daria.chebotnyagina@testpro.io");
+
+        // Step 3: Enter correct password
+        //WebElement passwordField = driver.findElement(By.cssSelector("input[type='password']"));
+        //passwordField.clear();
+        //passwordField.sendKeys("Asdfasdf1");
+
+        // Step 4: Click login button
+    clickSubmit();
+
+        WebElement avatar = driver.findElement(By.cssSelector("img[class='avatar']"));
+
+        // Expected result
+        Assert.assertTrue(avatar.isDisplayed());
+        //driver.quit();
+    }
+
 }
